10509bb6e3deddc7a7a9c7c15a31ca70974d223b
FarDean <sfh1212@gmail.com>
2021-03-16 20:15:59 +0330

    added counter

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
+import { useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import { Link } from "./components/Link";
 import { Str } from "./components/Str";
 
 function App() {
+	const [count, setCount] = useState(0);
+	function handleClick() {
+		setCount(pre => (pre += 1));
+	}
+	function handleClick2() {
+		setCount(pre => (pre -= 1));
+	}
 	return (
 		<div className="App">
 			<header className="App-header">
@@ -14,7 +22,9 @@ function App() {
 				<Link url="/" classname="App-link" variant="secondary">
 					learn react
 				</Link>
-				<Str name="fardean" age={27}></Str>
+				<Str handleClick2={handleClick2} handleClick={handleClick} name="fardean" age={27}>
+					{count}
+				</Str>
 			</header>
 		</div>
 	);
--- src/components/Str.tsx
+++ src/components/Str.tsx
@@ -3,13 +3,20 @@ import React from "react";
 interface KosProps {
 	name: string;
 	age: number;
+	handleClick: () => void;
+	handleClick2: () => void;
+	children: React.ReactNode;
 }
 
-export const Str: React.FC<KosProps> = ({ name, age }) => {
+export const Str: React.FC<KosProps> = ({ name, age, handleClick, children, handleClick2 }) => {
 	return (
 		<div>
 			<h1>Your name is: {name}</h1>
 			<h2>and you are {age} years old!</h2>
+			<h2>{children}</h2>
+
+			<button onClick={handleClick}>Increment</button>
+			<button onClick={handleClick2}>Decrement</button>
 		</div>
 	);
 };
